5ff417979d6dec224159297c949dc60e24ce0eb2
Senthil Kumar S A <sasenthilk@gmail.com>
2021-08-24 07:36:53 +0530

    ADDED: To display Weclome to Snake and Ladder Simulator

---
+++ src/com/bridgelabz/snakeandladderproblem/SnakeAndLadderSimulator.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.snakeandladderproblem;
+
+public class SnakeAndLadderSimulator {
+
+	public static void main(String[] args) {
+
+		System.out.println("----------Welcome to Snake And Ladder Simulator----------");
+	}
+}
