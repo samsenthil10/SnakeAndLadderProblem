0650cecc6caf049d01a00a44859bae4ea71b1031
Senthil Kumar S A <sasenthilk@gmail.com>
2021-08-24 07:50:05 +0530

    MODIFIED: To enable a player to selecta game state and move

--- src/com/bridgelabz/snakeandladderproblem/SnakeAndLadderSimulator.java
+++ src/com/bridgelabz/snakeandladderproblem/SnakeAndLadderSimulator.java
@@ -4,22 +4,25 @@ public class SnakeAndLadderSimulator {
 
 	public static void main(String[] args) {
 
-		int playerPosition=0;
+		int playerPosition = 0;
 		final int NO_PLAY = 0;
 		final int LADDER = 1;
 		final int SNAKE = 2;
-		int rollDie = (int)(Math.random() * 6)+1;
-		int movePlayer = (int)(Math.random()*3);
-		switch(movePlayer)
-		{
-		case NO_PLAY:System.out.println("No Play!"); 
-		break;
-		case LADDER :System.out.println("Ladder!"); 
-		playerPosition+=rollDie;
-		break;
-		case SNAKE : System.out.println("Snake");
-		playerPosition-=rollDie;
-		break;
+		while(playerPosition<100) {
+			int rollDie = (int)(Math.random() * 6)+1;
+			int movePlayer = (int)(Math.random()*3);
+			switch(movePlayer)
+			{
+			case NO_PLAY: 
+				break;
+			case LADDER : 
+				playerPosition+=rollDie;
+				break;
+			case SNAKE : 
+				playerPosition-=rollDie;
+				playerPosition= (playerPosition < 0) ? 0 : playerPosition ; 
+				break;
+			}
 		}
 		System.out.println("Single Player Current Position: "+playerPosition);
 	}
