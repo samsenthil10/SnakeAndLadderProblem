9b156c91a455d76a069c3ef967de286afcb5d975
Senthil Kumar S A <sasenthilk@gmail.com>
2021-08-24 07:45:58 +0530

    ADDED: To enable a player to selecta game state

--- src/com/bridgelabz/snakeandladderproblem/SnakeAndLadderSimulator.java
+++ src/com/bridgelabz/snakeandladderproblem/SnakeAndLadderSimulator.java
@@ -4,7 +4,23 @@ public class SnakeAndLadderSimulator {
 
 	public static void main(String[] args) {
 
-		int rollDie = (int)((Math.random()*6) +1);
-		System.out.println("Single Player Rolled Die Value : "+rollDie);
+		int playerPosition=0;
+		final int NO_PLAY = 0;
+		final int LADDER = 1;
+		final int SNAKE = 2;
+		int rollDie = (int)(Math.random() * 6)+1;
+		int movePlayer = (int)(Math.random()*3);
+		switch(movePlayer)
+		{
+		case NO_PLAY:System.out.println("No Play!"); 
+		break;
+		case LADDER :System.out.println("Ladder!"); 
+		playerPosition+=rollDie;
+		break;
+		case SNAKE : System.out.println("Snake");
+		playerPosition-=rollDie;
+		break;
+		}
+		System.out.println("Single Player Current Position: "+playerPosition);
 	}
 }
